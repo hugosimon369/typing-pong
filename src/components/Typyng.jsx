9555437ccadd347d29caf8499af1c4df713b6d0d
hugosimon369 <hugosimonalv@gmail.com>
2025-12-09 22:21:21 -0400

    v 0.2.0 Agregando objetivo simple. visualizacion en pantalla.

--- src/components/Typyng.jsx
+++ src/components/Typyng.jsx
@@ -3,24 +3,61 @@ import { useEffect, useState } from 'react'
 function TypingGame() {
 
     const [key, setKey] = useState()
-    
+
+    const [cursor, setCursor] = useState(0)
+
+    const [target, setTarget] = useState("hola mundo")
+
+    const [palabra, setPalabra] = useState("")
+
+    const [next, setNext] = useState(false)
+
+
     useEffect(() => {
         const handleKeyDown = (e) => {
             const id = e.key
             console.log("Tecla:", id); // Esto imprime la tecla 
             setKey(id)
+            if (id === target[cursor]) {
+                setCursor(cursor + 1)
+                setPalabra(palabra + target[cursor])
+            }
+            if (id === "Backspace") {
+                setCursor(0)
+                setPalabra("")
+            }
+            if (palabra === target) {
+                setNext(true)
+            } else { 
+                setNext(false) 
+            }
         };
+
         window.addEventListener('keydown', handleKeyDown);
+
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         };
-    }, []);
+    }, [cursor, palabra, key]);
 
     return (
         <main className='main' >
             <h1>Typing-pong</h1>
             <p>Presiona cualquier tecla...</p>
+            <p>Objetivo: <strong>' {target.split("").map((letra, index)=>{
+                let clase = "";
+                if(index < cursor) clase = "check"
+                if(index === cursor) clase = "target"
+                if( index > cursor) clase = ""
+                return(
+                    <span key={index} className={clase}>
+                        {letra}
+                    </span>
+                )
+            })} '</strong></p>
             <p>{key}</p>
+            <p>{palabra}</p>
+            {next && <button>avanzar</button>}
         </main>
     )
 }
