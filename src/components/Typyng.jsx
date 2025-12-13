f9cc52ff04c1674fcd142cca6d17eefe32dfc10c
hugosimon369 <hugosimonalv@gmail.com>
2025-12-12 22:12:20 -0400

    v 0.4.0 Cronometro. nuevo WPM. -error suma keyIncorrect al completar target, fixed ( pero+1 en la siguiente key)

--- src/components/Typyng.jsx
+++ src/components/Typyng.jsx
@@ -14,27 +14,62 @@ function TypingGame() {
 
     const [error, setError] = useState(false)
 
+    const [startTime, setStartTime] = useState(null)
+
+    const [wpm, setWpm] = useState(0)
+
+    const [keyIncorrect, setKeyIncorrect] = useState(0)
+
+    const [accuracy, setAccuracy] = useState()
+
+    const [timeInMinutes, setTimeInMinuetes] = useState()
+
+    const [timeLeft, setTimeLeft] = useState(600)
+
+    let netWpm = Math.max(0, wpm - (keyIncorrect / timeInMinutes))
+
+    let newNetWpm = (wpm * accuracy) / 100
+
+    let time = (60 - (timeLeft / 10)).toFixed(1)
 
     useEffect(() => {
         const handleKeyDown = (e) => {
             const id = e.key
             console.log("Tecla:", id); // Esto imprime la tecla 
             setKey(id)
+            if (startTime === null && id === target[cursor]) {
+                setStartTime(Date.now())
+            }
+            if (palabra === target) { // Verifica si ya termino el objetivo
+                setNext(true)
+                setStartTime(null)
+                console.log("target completado")
+            } else {
+                setNext(false)
+            }
             if (id === target[cursor]) {
                 setCursor(cursor + 1)
                 setPalabra(palabra + target[cursor])
                 setError(false)
+                let tiempo = (Date.now() - startTime) / 60000
+                setTimeInMinuetes(tiempo)
+                const keyTotal = keyIncorrect + cursor
+                const accuracy = (cursor / keyTotal) * 100
+                setAccuracy(accuracy)
+                setWpm((((cursor + 1) / 5) / tiempo))
             } else {
+                if (startTime) setKeyIncorrect((keyIncorrect + 1))
                 setError(true)
+                console.log(keyIncorrect + 1)
             }
             if (id === "Backspace") {
                 setCursor(0)
                 setPalabra("")
+                setKeyIncorrect(0)
+                setTimeLeft(600)
             }
-            if (palabra === target) {
-                setNext(true)
-            } else {
-                setNext(false)
+            if (startTime === null && id === target[cursor]) {
+                setStartTime(Date.now())
             }
         };
 
@@ -43,7 +78,25 @@ function TypingGame() {
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         };
-    }, [cursor, palabra, key]);
+    }, [cursor, palabra, key, keyIncorrect, startTime]);
+
+
+    useEffect(() => {
+        console.log(timeLeft) //verificacion de montaje
+        let interval
+        if (startTime && timeLeft > 0 && palabra !== target) {
+            interval = setInterval(() => {
+                setTimeLeft(timeLeft - 1)
+                console.log("restando 1 segundo")
+            }, 100);
+            return () => clearInterval(interval)
+        }
+
+
+
+    }, [timeLeft, startTime, setInterval])
+
+
 
     return (
         <main className='main' >
@@ -71,9 +124,16 @@ function TypingGame() {
                 })}
             </p>
             <p>{palabra}</p>
+            <p>tiempo: {time}</p>
+            <p>presicion: {accuracy}</p>
+            <p>WPM: {wpm}</p>
+            <p>net WPM {netWpm}</p>
+            <p>new net WPM: {newNetWpm}</p>
             {next && <button>avanzar</button>}
         </main>
     )
 }
+
+setInterval
 
 export default TypingGame
